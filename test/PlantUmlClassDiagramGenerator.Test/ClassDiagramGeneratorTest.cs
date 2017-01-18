0b7e4f1de87c461b2332fadd14c4a15a01c32689
codeyu <codeyu@gmail.com>
2017-01-18 14:24:37 +0800

    .net core

---
+++ test/PlantUmlClassDiagramGenerator.Test/ClassDiagramGeneratorTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using PlantUmlClassDiagramGenerator;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Text;
+using System.IO;
+using Xunit;
+namespace PlantUmlClassDiagramGeneratorTest
+{
+    public class ClassDiagramGeneratorTest
+    {
+        [Theory]
+        public void GenerateTest_All()
+        {
+            var code = File.ReadAllText("testData\\inputClasses.cs");
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var gen = new ClassDiagramGenerator(writer, "    ");
+                gen.Generate(root);
+            }
+
+            var expected = ConvertNewLineCode(File.ReadAllText(@"uml\all.puml"),Environment.NewLine);
+            var actual = output.ToString();
+            Console.Write(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        public void GenerateTest_Public()
+        {
+            var code = File.ReadAllText("testData\\inputClasses.cs");
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var gen = new ClassDiagramGenerator(writer, "    ",
+                    Accessibilities.Private | Accessibilities.Internal 
+                    | Accessibilities.Protected | Accessibilities.ProtectedInternal);
+                gen.Generate(root);
+            }
+
+            var expected = ConvertNewLineCode(File.ReadAllText(@"uml\public.puml"),Environment.NewLine);
+            var actual = output.ToString();
+            Console.Write(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        public void GenerateTest_WithoutPrivate()
+        {
+            var code = File.ReadAllText("testData\\inputClasses.cs");
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var gen = new ClassDiagramGenerator(writer, "    ",Accessibilities.Private);
+                gen.Generate(root);
+            }
+
+            var expected = ConvertNewLineCode( File.ReadAllText(@"uml\withoutPrivate.puml"), Environment.NewLine);
+            var actual = output.ToString();
+            Console.Write(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        public void GenerateTest_GenericsTypes()
+        {
+            var code = File.ReadAllText("testData\\GenericsType.cs");
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+
+            var output = new StringBuilder();
+            using (var writer = new StringWriter(output))
+            {
+                var gen = new ClassDiagramGenerator(writer, "    ", Accessibilities.Private | Accessibilities.Internal
+                    | Accessibilities.Protected | Accessibilities.ProtectedInternal);
+                gen.Generate(root);
+            }
+
+            var expected = ConvertNewLineCode(File.ReadAllText(@"uml\genericsType.puml"), Environment.NewLine);
+            var actual = output.ToString();
+            Console.Write(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        private string ConvertNewLineCode(string text,string newline)
+        {
+            var reg = new System.Text.RegularExpressions.Regex("\r\n|\r|\n");
+            return reg.Replace(text, newline);
+        }
+    }
+}
---
+++ test/PlantUmlClassDiagramGenerator.Test/testData/GenericsType.cs
@@ -0,0 +1,30 @@
+﻿namespace PlantUmlClassDiagramGeneratorTest
+{
+    class GenericsType
+    {
+        public object Value { get; }
+    }
+
+    class GenericsType<T>
+    {
+        public T Value { get; }
+    }
+
+    class GenericsType<T1, T2>
+    {
+        public T1 Value1 { get; }
+        public T2 Value2 { get; }
+    }
+
+    class SubClass : GenericsType<string, int>
+    {
+        public new string Value1 { get; }
+        public new int Value2 { get; }
+    }
+
+    class SubClass<T>: GenericsType<GenericsType<int>, T>
+    {
+        public new GenericsType<int> Value1 { get; }
+        public new T Value2 { get; }
+    }
+}
